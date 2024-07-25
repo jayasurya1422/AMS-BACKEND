96589e94ad89f774bfbb493c7d7c16e84a09a1e2
Jayasurya B V S <164497906+jayasurya1422@users.noreply.github.com>
2024-07-25 13:53:07 +0530

    Update PondDetailData.js

--- controllers/PondDetailData.js
+++ controllers/PondDetailData.js
@@ -1,87 +1,66 @@
-const db = require('../firebase'); // Import Firestore instance
-const firebase = require('firebase/app'); // Import firebase to use serverTimestamp()
+const Pond = require('../models/pond.model');
 
-const sensordataHandler = async (req, res, next) => {
-  const { pondId, i1, i2 } = req.body;
-
-  let exists = false;
-  let sensordata;
+const savePondHandler = async (req, res) => {
+  const { receivedString } = req.body;
+  const newPond = new Pond(receivedString);
   try {
-    const sensordataSnapshot = await db.collection('ponddetail').get();
-    sensordata = sensordataSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    if (sensordata.length === 1) {
-      exists = true;
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Updating Data Failed",
-    });
+    await Pond.savePondDetail(newPond);
+    res.status(200).json({ message: 'Pond saved successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Saving pond detail failed.', error });
   }
-
-  if (exists) {
-    try {
-      await db.collection('ponddetail').doc(sensordata[0].id).update({
-        pondId: pondId,
-        i1: i1,
-        i2: i2,
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Updating Data Failed",
-      });
-    }
-  } else {
-    try {
-      await db.collection('ponddetail').add({
-        pondId: pondId,
-        i1: i1,
-        i2: i2,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Updating Data Failed",
-      });
-    }
-  }
-
-  return res.status(200).json({
-    message: "Data updated successfully",
-  });
 };
 
-const getdataHandler = async (req, res, next) => {
-  let sensordata;
+const getPondHandler = async (req, res) => {
   try {
-    const sensordataSnapshot = await db.collection('ponddetail').get();
-    sensordata = sensordataSnapshot.docs.map(doc => doc.data());
-    console.log(sensordata);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Getting Data Failed",
-    });
+    const pondDetail = await Pond.getPondDetail(req.id);
+    if (pondDetail) {
+      res.status(200).json(pondDetail);
+    } else {
+      res.status(404).json({ message: 'Pond not found.' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Getting pond detail failed.', error });
   }
-
-  if (sensordata.length === 0) {
-    return res.status(404).json({
-      message: "No sensor data found",
-    });
-  }
-
-  const data = {
-    pondId: sensordata[0].pondId,
-    i1: sensordata[0].i1,
-    i2: sensordata[0].i2,
-  };
-
-  return res.status(200).json(data);
 };
 
-exports.ponddataHandler = ponddataHandler;
-exports.getdataHandler = getdataHandler;
+const getAllPondsHandler = async (req, res) => {
+  try {
+    const ponds = await Pond.getAllPonds();
+    res.status(200).json(ponds);
+  } catch (error) {
+    res.status(500).json({ message: 'Getting all ponds failed.', error });
+  }
+};
+
+const deletePondHandler = async (req, res) => {
+  try {
+    await Pond.deletePond(req.params.pondId);
+    res.status(200).json({ message: 'Pond deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Deleting pond failed.', error });
+  }
+};
+
+const updatePondHandler = async (req, res) => {
+  try {
+    const pondDetail = await Pond.getPondDetail(req.params.pondId);
+    if (pondDetail) {
+      const updatedPond = new Pond(req.body.receivedString);
+      await Pond.savePondDetail(updatedPond);
+      res.status(200).json({ message: 'Pond updated successfully.' });
+    } else {
+      res.status(404).json({ message: 'Pond not found.' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Updating pond detail failed.', error });
+  }
+};
+
+module.exports = {
+  savePondHandler,
+  getPondHandler,
+  getAllPondsHandler,
+  deletePondHandler,
+  updatePondHandler
+};
